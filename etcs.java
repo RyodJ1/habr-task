417154e52893a4add8694a249f716202eeb8165d
RyodJ1 <126905554+RyodJ1@users.noreply.github.com>
2024-03-20 18:43:47 +0600

    Add files via upload

---
+++ etcs.java
@@ -0,0 +1,19 @@
+public class etcs {
+    public static void main(String[] args) {
+        double[] array = {3.5, 2.0, 1.0, 4.0}; 
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    double temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
